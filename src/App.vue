d169c6b13066b142bf20d0010ce5091e1963c071
AgungAwan <agungkristiawan24@gmail.com>
2023-05-18 16:06:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import QrcodeVue from 'qrcode.vue'
+
+const data = reactive({
+  QrValue: '',
+  size: 300,
+})
+
+
+
+</script>
+
+<template>
+  <header>
+    <div class="">
+      <p>QR Value</p>
+      <!-- <input type="text" class="input-qr-value" id="qr-content" v-model="data.QrValue"> -->
+      <textarea name="" id="" cols="50" rows="5" v-model="data.QrValue" placeholder="input "></textarea>
+    </div>
+  </header>
+
+  <main>
+    <div class="">
+      <qrcode-vue :value="data.QrValue" :size="data.size" level="H" />
+      <br>
+      {{ data.QrValue }}
+    </div>
+
+  </main>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.input-qr-value {
+  /* margin: 0.5rem; */
+  width: 20vw;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
