384dc88ae0c3ba46655662259bde7ab9b77a93df
AgungAwan <agungkristiawan24@gmail.com>
2023-05-18 21:01:08 +0800

    add download functionality

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
+import html2canvas from 'html2canvas'
 import QrcodeVue from 'qrcode.vue'
 
 const data = reactive({
@@ -7,7 +8,33 @@ const data = reactive({
   size: 300,
 })
 
+const printcontent = ref<HTMLDivElement>()
 
+async function printThis() {
+  console.log("printing..");
+  const el = printcontent.value;
+
+  console.log('el', el);
+  if (!el) return;
+
+  const options = {
+    type: "dataURL"
+  };
+
+  const printCanvas = await html2canvas(el);
+
+  const link = document.createElement("a");
+  link.setAttribute("download", "myQR.png");
+  link.setAttribute(
+    "href",
+    printCanvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream")
+  );
+  link.click();
+
+  console.log("done");
+}
 
 </script>
 
@@ -16,19 +43,19 @@ const data = reactive({
     <div class="">
       <p>QR Value</p>
       <!-- <input type="text" class="input-qr-value" id="qr-content" v-model="data.QrValue"> -->
-      <textarea name="" id="" cols="50" rows="5" v-model="data.QrValue" placeholder="input "></textarea>
+      <textarea name="" id="" class="input-qr-value " cols="50" rows="5" v-model="data.QrValue" placeholder="input "></textarea>
     </div>
   </header>
 
   <main>
-    <div class="">
+    <div class="" ref="printcontent" style="width: fit-content;">
       <qrcode-vue :value="data.QrValue" :size="data.size" level="H" />
-      <br>
-      {{ data.QrValue }}
     </div>
-
+    <br>
+    {{ data.QrValue }}
+    <br>
+    <button :onclick="printThis" style="background-color: green;">download</button>
   </main>
-
 </template>
 
 <style scoped>
@@ -42,8 +69,7 @@ header {
 }
 
 .input-qr-value {
-  /* margin: 0.5rem; */
-  width: 20vw;
+  margin-bottom: 1rem;
 }
 
 @media (min-width: 1024px) {
